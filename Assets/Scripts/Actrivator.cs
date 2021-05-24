3d54f2ad729f255e7b41f509a8289eed200905f7
llozpk <igor7713hotnews@gmail.com>
2021-05-24 09:07:46 -0300

    Aki

---
+++ Assets/Scripts/Actrivator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  public class Actrivator : MonoBehaviour{
+
+    public GameObject tongue;
+
+  void Start ()
+  {
+      InvokeRepeating("TongueOut", 15f, 1f);
+  }
+
+  void TongueOut ()
+  {
+      tongue.SetActive(true);
+      Invoke("TongueIn", 0.5f);
+  }
+
+  void TongueIn ()
+  {
+      tongue.SetActive(false);
+  }
+}
---
+++ Assets/Scripts/ButtonCollorChange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCollorChange : MonoBehaviour{
+
+  public Button button;
+  private bool clicked = true;
+
+  void Start () {
+    Button btn = button.GetComponent<Button>();
+    btn.onClick.AddListener(TaskOnClick);
+  }
+
+  void TaskOnClick(){
+      if (clicked) {
+        TurnRed();
+        clicked = false;
+      }else{
+        TurnWhite();
+        clicked = true;
+      }
+
+	}
+
+  public void TurnRed()
+     {
+         ColorBlock colors = button.colors;
+         colors.normalColor = Color.green;
+         colors.highlightedColor = new Color32(100, 255, 100, 255);
+         button.GetComponent<Image>().color = Color.green;
+         button.colors = colors;
+     }
+
+     public void TurnWhite()
+     {
+         ColorBlock colors = button.colors;
+         colors.normalColor = Color.white;
+         colors.highlightedColor = new Color32(225, 225, 225, 255);
+         button.GetComponent<Image>().color = Color.white;
+         button.colors = colors;
+     }
+}
